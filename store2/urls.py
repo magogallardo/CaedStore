f8379c9041166a2b5c4ca34fccd3b485947bb3d7
Mago Gallardo <gallardo.dominguez.emmanuel@gmail.com>
2019-05-27 22:37:23 -0500

    please stop

--- store2/urls.py
+++ store2/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
   url(r'^login/$', views.login, name="login"),
   url(r'^singup/$', views.singup, name="singup"),
   url(r'^singup/name=(?P<name>[\s\S]+)&last_name=(?P<last_name>[\s\S]+)&email=(?P<email>[\s\S]+)&phone=(?P<phone>[\s\S]+)&user_name=(?P<user_name>[\s\S]+)&psw=(?P<password>[\s\S]+)&psw=([\s\S]+)&singupButton=Acceder', views.singup2, name = "singUp2"),
-  url(r'^\?search_text=+(?P<search_text>[\s\S]+)&search=Ok', views.search, name = "search")
+  url(r'^search_text=+(?P<search_text>[\s\S]+)&search=Ok', views.search, name = "search")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
